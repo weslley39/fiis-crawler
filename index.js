1e536c8d6fb9e820bdf1b660405151ef605b44af
Weslley Neri <weslley39@gmail.com>
2019-01-19 13:27:28 -0200

    First Coomit 🚀

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Hapi = require('hapi');
+const cheerio = require('cheerio');
+const requestPromise = require('request-promise');
+
+const FIIS_URL = 'https://fiis.com.br';
+const PREFIX = '/api'
+
+const server = Hapi.server({
+    port: process.env.PORT ||3010
+});
+
+server.route({
+  method:'GET',
+  path:'/equity-per-share',
+  handler: async (request, h) => {
+
+      return 'Running 🏃🏻‍♂️';
+  }
+});
+
+server.route({
+    method:'GET',
+    path: `${PREFIX}/equity-per-share`,
+    handler: async (request, reply) => {
+
+        const { name } = request.query;
+        let value;
+        try {
+          const html = await requestPromise(`${FIIS_URL}/${name}/?aba=indicadores`)
+          const $ = cheerio.load(html);
+
+          const tr = $('table tr').eq(11).text();
+          const texts = tr.split('*');
+          value = texts[0].match(/[0-9].*/g)[0];
+        } catch (error) {
+          value = `${name} NOT FOUND`
+        }
+
+        return { value };
+    }
+});
+
+// Start the server
+const start =  async function() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log(`The magic happens on: ${server.info.uri}`,);
+};
+
+start();
